2199031177b9530a328e4a810f340478e13709dd
nvmburritos <ujferenc97@gmail.com>
2023-04-20 13:14:45 +0200

    Test fix,

--- SRBackend/test/test_user_delete.py
+++ SRBackend/test/test_user_delete.py
@@ -2,5 +2,5 @@ from test.conftest import get_client
 
 def test_can_call_endpoint():
     r = get_client().delete('/user/asddwdawd')
-    assert r.status_code == 404
+    assert r.status_code == 400
     pass
